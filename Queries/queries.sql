16f8b06346c679c2afb81545b0eb0933c53e7d9d
Tyler-Foreman <75589171+Tyler-Foreman@users.noreply.github.com>
2021-01-27 16:33:04 -0600

    Updated queries/schema. Added employee count by department.

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -44,3 +44,61 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Select all from retirement_info table
 SELECT * FROM retirement_info;
+
+-- Drop retirement_info table to recreate it with emp_no column
+DROP TABLE retirement_info;
+
+-- Create a new table for retiring employees by department
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Joining departments and dept_manager tables [practice]
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+	retirement_info.last_name,
+	dept_employees.to_date
+FROM retirement_info
+LEFT JOIN dept_employees
+ON retirement_info.emp_no = dept_employees.emp_no;
+
+-- Practicing aliases on retirement_info (ri) and dept_employees(de) tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_employees as de
+ON ri.emp_no = de.emp_no;
+
+-- Making sure that employees still work for PH and creating a new current_emp table
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employees as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Getting employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_count_dept
+FROM current_emp as ce
+LEFT JOIN dept_employees as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
