80891a2bac01d141eca0093a61e7d7f4d9aa65f1
Tyler-Foreman <75589171+Tyler-Foreman@users.noreply.github.com>
2021-01-29 22:53:10 -0600

    Adding mentorship eligibility SQL and CSV data.

--- SQL/Employee_Database_Challenge.sql
+++ SQL/Employee_Database_Challenge.sql
@@ -35,4 +35,22 @@ GROUP BY title
 ORDER BY "count" DESC;
 
 
+-- Create mentorship_eligibility table 
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibity
+FROM employees AS e
+JOIN dept_employees AS de
+	ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
 
